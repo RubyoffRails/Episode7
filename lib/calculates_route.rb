ff6583964ea570227533c7314c8831f85d01c352
Jesse Wolgamott <jesse@comalproductions.com>
2012-12-26 10:38:49 -0600

    Use spaces, not tabs

--- lib/calculates_route.rb
+++ lib/calculates_route.rb
@@ -1,22 +1,22 @@
 class CalculatesRoute
 
-	def self.calculate(points)
+  def self.calculate(points)
 
-		remaining_points = points
-		route = []
-		route << remaining_points.slice!(0)
-		until remaining_points == [] do 
-			next_point = shortest_distance(route.last, remaining_points)
-			route << remaining_points.slice!(remaining_points.index(next_point))
-		end
-		route
-	end
+    remaining_points = points
+    route = []
+    route << remaining_points.slice!(0)
+    until remaining_points == [] do 
+      next_point = shortest_distance(route.last, remaining_points)
+      route << remaining_points.slice!(remaining_points.index(next_point))
+    end
+    route
+  end
 
-	def self.shortest_distance(from, possible)
-		distances = possible.map do |point|
-			{point: point, distance: Map.distance_between(from, point)}
-		end
-		distances.sort{|a,b| a.fetch(:distance) <=> b.fetch(:distance)}.first.fetch(:point)
-	end
+  def self.shortest_distance(from, possible)
+    distances = possible.map do |point|
+      {point: point, distance: Map.distance_between(from, point)}
+    end
+    distances.sort{|a,b| a.fetch(:distance) <=> b.fetch(:distance)}.first.fetch(:point)
+  end
 end
 
--- lib/place.rb
+++ lib/place.rb
@@ -1,21 +1,21 @@
 require_relative "./map"
 
 class Place
-	attr_accessor :name, :coordinates
+  attr_accessor :name, :coordinates
 
-	def self.build(name)
-		results = Map.search(name)
-		Place.new.tap do |p|
-			p.name = name
-			p.coordinates = results.coordinates
-		end
-	end
+  def self.build(name)
+    results = Map.search(name)
+    Place.new.tap do |p|
+      p.name = name
+      p.coordinates = results.coordinates
+    end
+  end
 
-	def to_s
-		name
-	end
+  def to_s
+    name
+  end
 
-	def to_coordinates
-		coordinates
-	end
+  def to_coordinates
+    coordinates
+  end
 end
